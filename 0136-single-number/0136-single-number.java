5cf3f00155a1c1a8642a312dab09ac8a6137ed33
Nischith11 <140196498+Nischith11@users.noreply.github.com>
2024-07-03 18:11:06 +0530

    Time: 1 ms (99.89%), Space: 46.1 MB (24.23%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,28 @@
 import java.util.*;
 class Solution {
     public int singleNumber(int[] array) {
-        int ans=-1;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<array.length;i++){
-            int value=map.getOrDefault(array[i],0);
-            map.put(array[i],value+1);
+        //better solution
+        // int ans=-1;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<array.length;i++){
+        //     int value=map.getOrDefault(array[i],0);
+        //     map.put(array[i],value+1);
+        // }
+        // //for(Integer it:arr)
+        // for(Map.Entry<Integer,Integer> it:map.entrySet()){
+        //     int freq=it.getValue();
+        //     if(freq == 1){
+        //         ans=it.getKey();
+        //     }
+        // }
+        // return ans;
+        
+        //optimal solution
+        int x =0;
+        for(int i=0;i<array.length;i++)
+        {
+            x = x^array[i];
         }
-        //for(Integer it:arr)
-        for(Map.Entry<Integer,Integer> it:map.entrySet()){
-            int freq=it.getValue();
-            if(freq == 1){
-                ans=it.getKey();
-            }
-        }
-        return ans;
+        return x;
     }
 }
