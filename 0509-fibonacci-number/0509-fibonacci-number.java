0cc94cc2bf91ddfeff6d1c3636457807b041059b
Nischith11 <140196498+Nischith11@users.noreply.github.com>
2024-07-18 17:19:14 +0530

    Time: 8 ms (50.43%), Space: 40.1 MB (67.88%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,23 +1,32 @@
 class Solution {
     public int fib(int n) {
         //without using recursion
-        if(n == 0){
-            return 0;
+        // if(n == 0){
+        //     return 0;
+        // }
+        // if(n == 1){
+        //     return 1;
+        // }
+        // int a=0;
+        // int b=1;
+        // int count=1;
+        // while(true){
+        //     int c=a+b;
+        //     count++;
+        //     if(count == n){
+        //         return c;
+        //     }
+        //     int temp=b;
+        //     a=b;b=c;
+        // }
+       return  nthFibo(n);
+    }
+      //solution 2 --> using recursion
+      //create a method that will calculate the nth fibonacci number
+    public int nthFibo(int n){
+        if(n == 0 || n == 1){
+            return n;
         }
-        if(n == 1){
-            return 1;
-        }
-        int a=0;
-        int b=1;
-        int count=1;
-        while(true){
-            int c=a+b;
-            count++;
-            if(count == n){
-                return c;
-            }
-            int temp=b;
-            a=b;b=c;
-        }
+        return nthFibo(n-1) + nthFibo(n-2);
     }
 }
