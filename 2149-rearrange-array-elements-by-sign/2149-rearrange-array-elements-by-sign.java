a1dca7b5736cfb4b945eb76743c9c230a7ad8771
Nischith11 <140196498+Nischith11@users.noreply.github.com>
2024-08-04 19:10:37 +0530

    Time: 3 ms (100.00%), Space: 76.7 MB (78.15%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -13,5 +13,17 @@ class Solution {
             }
         }
         return ans;
+        
+        //what if this was the case 
+        //same problem but
+        //Number of positives != Number of negatives 
+        //Then thier is only one solution
+        //Take 2 ArrayList
+        //One is for +ve's and the other one is for -ve's
+        //if this was the case then 
+        //If no's of +ve's < than the No's of -ve's
+        //first fill array alternatively till the point
+        //fill remaining -ve's at the end 
+        //vice-versa
     }
 }
